fd9eeb742a5e90ecfae52853f840f2a4b491d9cf
Julia Bandurska <julka.ban@gmail.com>
2024-01-08 21:13:13 +0100

    Deletion of threads on user deletion

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import Thread from "./thread.model.js";
 
 const userSchema = new Schema({
   login: {
@@ -28,6 +29,19 @@ const userSchema = new Schema({
     required: true,
     default: [],
   },
+});
+
+userSchema.pre("findOneAndDelete", async function (next) {
+  const doc = await this.model.findOne(this.getQuery());
+
+  if (doc) {
+    await Thread.deleteMany({
+      user: doc._id,
+      isMainThread: true,
+    });
+  }
+
+  next();
 });
 
 export default model("User", userSchema);
